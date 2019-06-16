f3182f944810f4dfaa3a1a8c6e1e9b2d0f524585
AbbasHassan <abbashassan01@outlook.com>
2019-06-16 15:47:23 +0500

    File chooser fixed

--- app/src/main/java/set/fabsolutions/shadfy/Home.java
+++ app/src/main/java/set/fabsolutions/shadfy/Home.java
@@ -1,25 +1,55 @@
 package set.fabsolutions.shadfy;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.provider.Settings;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.PermissionRequest;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.single.CompositePermissionListener;
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.karumi.dexter.listener.single.SnackbarOnDeniedPermissionListener;
 import com.onesignal.OneSignal;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 
 
 public class Home extends AppCompatActivity {
 
     private WebView mWebView;
+    private ValueCallback<Uri[]> filePathCallbackX;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -31,10 +61,6 @@ public class Home extends AppCompatActivity {
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init();
         setContentView(R.layout.activity_home);
-
-
-
-
 
 
         //Web View
@@ -56,34 +82,79 @@ public class Home extends AppCompatActivity {
         webSettings.setSavePassword(true);
         webSettings.setSaveFormData(true);
         webSettings.setEnableSmoothTransition(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setDatabaseEnabled(true);
 
         mWebView.loadUrl("https://www.shadfy.com/mobile/#/intro");
 
-
-
-
         //force links open in webview only
         mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(final WebView webView, final ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                Log.i("mytag", "on file chooser");
+
+                filePathCallbackX = filePathCallback;
+                Dexter.withActivity(Home.this)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse response) {
+                                showFileChooser();
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse response) {
+                                Snackbar snackbar = Snackbar.make(webView, "Storage access is needed to pick image from gallery", 5000);
+                                if (response.isPermanentlyDenied()) {
+                                    snackbar.setAction("Settings", new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            openSettings();
+                                        }
+                                    });
+                                } else {
+                                    snackbar.setAction("OK", new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+
+                                        }
+                                    });
+                                }
+                                snackbar.show();
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(com.karumi.dexter.listener.PermissionRequest permission, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+                        })
+                        .check();
+                return true;
+            }
+        });
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, 57);
     }
 
     private class MyWebViewClient extends WebViewClient {
 
-
-
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
 
-            if (Uri.parse(url).getHost().equals("shadfy.com"))
-            {
+            if (Uri.parse(url).getHost().equals("shadfy.com")) {
                 return false;
-            }
-            else {
+            } else {
                 //here open external links in external app or browser
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                 startActivity(intent);
                 return true;
             }
         }
+
 
         //progress Dialogue
         ProgressDialog progressDialog = null;
@@ -99,28 +170,39 @@ public class Home extends AppCompatActivity {
 
         @Override
         public void onPageFinished(WebView view, String url) {
-
-           progressDialog.dismiss();
+            progressDialog.dismiss();
             super.onPageFinished(view, url);
         }
+    }
 
+    public void showFileChooser() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(Home.this);
+        /*Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+
+        // Update with mime types
+        intent.setType("image/*");
+
+        // Only pick openable and local files. Theoretically we could pull files from google drive
+        // or other applications that have networked files, but that's unnecessary for this example.
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
+        // REQUEST_CODE = <some-integer>
+        startActivityForResult(intent, 58);*/
     }
 
     //goto previous page when press back button
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (event.getAction()== KeyEvent.ACTION_DOWN)
-        {
-            switch (keyCode)
-            {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
                 case KeyEvent.KEYCODE_BACK:
-                    if (mWebView.canGoBack())
-                    {
+                    if (mWebView.canGoBack()) {
                         mWebView.goBack();
-                    }
-                    else
-                    {
+                    } else {
                         finish();
                     }
                     return true;
@@ -129,4 +211,59 @@ public class Home extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 57) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                showFileChooser();
+            }
+        } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                final Uri resultUri = result.getUri();
+                filePathCallbackX.onReceiveValue(new Uri[]{resultUri});
+                /*Log.i("mytag", "file path: " + resultUri.getPath());
+                File imageFile = new File(resultUri.getPath());*/
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private String getFileName(Uri uri) throws IllegalArgumentException {
+        //String[] proj = {MediaStore.Images.Media.DATA};
+        // Obtain a cursor with information regarding this uri
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+
+        if (cursor.getCount() <= 0) {
+            cursor.close();
+            throw new IllegalArgumentException("Can't obtain file name, cursor is empty");
+        }
+
+        cursor.moveToFirst();
+
+        String path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA));
+
+        //String fileName = cursor.getString(cursor.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME));
+
+        cursor.close();
+
+        return path;
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
 }
